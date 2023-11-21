2ad3c44bd727ce249aae250de503f5c46046a067
MOHAMMAD TALHA ALI <talhaali0006969@gmail.com>
2023-11-21 11:05:24 +0500

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:emart_app/views/splash_screen/splashScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:get/get.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 import 'consts/consts.dart';
 
-void main() {
+void main() async {
+  Stripe.publishableKey = publishableKey;
+  WidgetsFlutterBinding.ensureInitialized();
+  ByteData data =
+      await PlatformAssetBundle().load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext
+      .setTrustedCertificatesBytes(data.buffer.asUint8List());
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,9 +23,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: appname,
-      theme: ThemeData(),
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.transparent,
+          appBarTheme: const AppBarTheme(
+            iconTheme: IconThemeData(
+              color: darkFontGrey,
+            ),
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
+          ),
+          fontFamily: regular),
+      home: SplashScreen(),
     );
   }
 }
